5b7127b967fcd374cb89443239fde0b8c7bd0be7
Nguyencoder11 <lenhnguyen10a22003@gmail.com>
2023-05-20 14:09:31 +0700

    first commit

---
+++ Phieu_giao_BT3.cpp
@@ -0,0 +1,65 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+class Phanso
+{
+private:
+    int Tuso, Mauso;
+
+public:
+    Phanso()
+    {
+        Tuso = 1;
+        Mauso = 2;
+    }
+
+    Phanso(int Tuso, int Mauso)
+    {
+        this->Tuso = Tuso;
+        this->Mauso = Mauso;
+    }
+
+    friend istream &operator>>(istream &is, Phanso &ps)
+    {
+        cout << "Tu so: ";
+        is >> ps.Tuso;
+        cout << "Mau so: ";
+        is >> ps.Mauso;
+        return is;
+    }
+
+    friend ostream &operator<<(ostream &os, Phanso ps)
+    {
+        os << ps.Tuso << "/" << ps.Mauso;
+        return os;
+    }
+
+    Phanso operator*(Phanso y)
+    {
+        Phanso c;
+        c.Tuso = Tuso * y.Tuso;
+        c.Mauso = Mauso * y.Mauso;
+        return c;
+    }
+
+    float operator++()
+    {
+        return (float)Tuso / Mauso;
+    }
+};
+
+int main()
+{
+    Phanso a, b;
+    cout << "Nhap phan so A\n";
+    cin >> a;
+    cout << "Nhap phan so B\n";
+    cin >> b;
+    Phanso c = a.operator*(b);
+    cout << "Tich cua A va B: " << c << endl;
+    ofstream f("PHANSO.txt", ios::out);
+    f << "Phan so A: " << a << endl;
+    f << "Phan so B: " << b << endl;
+    f << "Tich cua A va B: " << c << endl;
+    f << "Gia tri cua phan so tich: " << ++c << endl;
+}
